297f5d4147bfcf64d7bb9e8f3cb4b1136e664796
Drata-Action-Bot-QA <actionbot-drata@drata.com>
2024-05-30 03:47:35 +0000

    Drata Compliance Recommendations (Grouped)

--- terraform/aws/modules/storage/main.tf
+++ terraform/aws/modules/storage/main.tf
@@ -29,6 +29,7 @@ resource "aws_security_group" "km_rds_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  # Drata: Ensure that [aws_security_group.egress.cidr_blocks] is explicitly defined and narrowly scoped to only allow traffic to trusted sources
   }
 }
 
