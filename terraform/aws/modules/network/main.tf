a1fd64be4d29be44fd4301e80fbec1eb00237447
Drata-Action-Bot-QA <actionbot-drata@drata.com>
2024-07-02 03:51:24 +0000

    Drata Compliance Recommendations: Network Configurations Restrict Broad Access

--- terraform/aws/modules/network/main.tf
+++ terraform/aws/modules/network/main.tf
@@ -146,6 +146,7 @@ resource "aws_security_group" "km_ecs_sg" {
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
+  # Drata: Ensure that [aws_security_group.egress.cidr_blocks] is explicitly defined and narrowly scoped to only allow traffic to trusted sources
   }
 
   tags = merge(var.default_tags, {
